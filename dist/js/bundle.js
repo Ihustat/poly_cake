bd4c83ccbb0fcf2fe95005940dd8d82acb88f2f4
Ihustat <ger26dan@gmail.com>
2023-11-29 15:46:52 +0300

    rework reviews slider

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -537,96 +537,94 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const reviewsSlider = () => {
-    const slides = document.querySelectorAll('.reviews__slide'),
+  const slides = document.querySelectorAll('.reviews__slide'),
     dotsContainer = document.querySelector('.reviews__dots'),
     dots = [];
 
-    let dotsCounter = 0;
-    let sliderTimer = setInterval(() => {
-        changeSlide(++dotsCounter);
-    }, 5000);
-    let touchStart, touchEnd;
+  let dotsCounter = 0;
+  let sliderTimer = setInterval(() => {
+    changeSlide(++dotsCounter);
+  }, 5000);
+  let touchStart, touchEnd;
 
-    function changeSlideByTouch() {
-console.log(touchStart, touchEnd)
-        if (touchStart > touchEnd) {
-            changeSlide(dotsCounter + 1);
-        } else {
-            changeSlide(dotsCounter - 1);
-        };
-    };
+  function changeSlideByTouch() {
+    if (touchStart > touchEnd) {
+      changeSlide(dotsCounter + 1);
+    } else {
+      changeSlide(dotsCounter - 1);
+    }
+  }
 
-    slides.forEach(() => {
+  slides.forEach(() => {
     const dot = document.createElement('div');
     dot.classList.add('dot');
 
     dots.push(dot);
 
     dotsContainer.append(dot);
+  });
+
+  slides.forEach((slide) => {
+    slide.addEventListener('mouseenter', () => {
+      clearInterval(sliderTimer);
     });
 
-    slides.forEach(slide => {
-        slide.addEventListener('mouseenter', () => {
-            clearInterval(sliderTimer);
-        });
+    slide.addEventListener('touchstart', (e) => {
+      touchStart = e.changedTouches[0].screenX;
+      clearInterval(sliderTimer);
+    });
+  });
 
-        slide.addEventListener('touchstart', (e) => {
-            e.preventDefault();
-            touchStart = e.changedTouches[0].screenX;
-            clearInterval(sliderTimer);
-        });
+  slides.forEach((slide) => {
+    slide.addEventListener('mouseleave', () => {
+      sliderTimer = setInterval(() => {
+        changeSlide(++dotsCounter);
+      }, 5000);
     });
 
-    slides.forEach(slide => {
-        slide.addEventListener('mouseleave', () => {
-            sliderTimer = setInterval(() => {
-                changeSlide(++dotsCounter);
-            }, 5000);
-        });
+    slide.addEventListener('touchend', (e) => {
+      e.preventDefault();
+      touchEnd = e.changedTouches[0].screenX;
+      changeSlideByTouch();
+      sliderTimer = setInterval(() => {
+        changeSlide(++dotsCounter);
+      }, 5000);
+    });
+  });
 
-        slide.addEventListener('touchend', (e) => {
-            e.preventDefault();
-            touchEnd = e.changedTouches[0].screenX;
-             changeSlideByTouch();
-             sliderTimer = setInterval(() => {
-                changeSlide(++dotsCounter);
-            }, 5000);
-         });
+  function changeSlide(n) {
+    dotsCounter = n;
+
+    dotsCounter = (0,_checkCounter__WEBPACK_IMPORTED_MODULE_0__["default"])(dotsCounter, slides.length - 1);
+
+    dots.forEach((dot) => {
+      dot.classList.remove('dot_active');
     });
 
-    function changeSlide(n) {
-        dotsCounter = n;
+    dots[dotsCounter].classList.add('dot_active');
 
-        dotsCounter = (0,_checkCounter__WEBPACK_IMPORTED_MODULE_0__["default"])(dotsCounter, slides.length - 1);
-       
-        dots.forEach(dot => {
-            dot.classList.remove('dot_active');
-        });
+    slides.forEach((slide) => {
+      slide.style.opacity = '0';
+    });
 
-        dots[dotsCounter].classList.add('dot_active');
+    slides[dotsCounter].style.opacity = '1';
+  }
 
-        slides.forEach(slide => {
-            slide.style.opacity = '0';
-        });
-
-        slides[dotsCounter].style.opacity = '1';
-    };
-
-    dotsContainer.addEventListener('click', (e) => {
+  dotsContainer.addEventListener('click', (e) => {
     const target = e.target;
 
     dots.forEach((dot, i) => {
-        if (target && dot === target) {
-                changeSlide(i);
-            };
-        });
+      if (target && dot === target) {
+        changeSlide(i);
+      }
     });
+  });
 
-    dots[0].click();
-
+  dots[0].click();
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (reviewsSlider);
+
 
 /***/ }),
 
--- src/js/modules/reviewsSlider.js
+++ src/js/modules/reviewsSlider.js
@@ -1,93 +1,90 @@
-import checkCounter from "./checkCounter";
+import checkCounter from './checkCounter';
 
 const reviewsSlider = () => {
-    const slides = document.querySelectorAll('.reviews__slide'),
+  const slides = document.querySelectorAll('.reviews__slide'),
     dotsContainer = document.querySelector('.reviews__dots'),
     dots = [];
 
-    let dotsCounter = 0;
-    let sliderTimer = setInterval(() => {
-        changeSlide(++dotsCounter);
-    }, 5000);
-    let touchStart, touchEnd;
+  let dotsCounter = 0;
+  let sliderTimer = setInterval(() => {
+    changeSlide(++dotsCounter);
+  }, 5000);
+  let touchStart, touchEnd;
 
-    function changeSlideByTouch() {
-console.log(touchStart, touchEnd)
-        if (touchStart > touchEnd) {
-            changeSlide(dotsCounter + 1);
-        } else {
-            changeSlide(dotsCounter - 1);
-        };
-    };
+  function changeSlideByTouch() {
+    if (touchStart > touchEnd) {
+      changeSlide(dotsCounter + 1);
+    } else {
+      changeSlide(dotsCounter - 1);
+    }
+  }
 
-    slides.forEach(() => {
+  slides.forEach(() => {
     const dot = document.createElement('div');
     dot.classList.add('dot');
 
     dots.push(dot);
 
     dotsContainer.append(dot);
+  });
+
+  slides.forEach((slide) => {
+    slide.addEventListener('mouseenter', () => {
+      clearInterval(sliderTimer);
     });
 
-    slides.forEach(slide => {
-        slide.addEventListener('mouseenter', () => {
-            clearInterval(sliderTimer);
-        });
+    slide.addEventListener('touchstart', (e) => {
+      touchStart = e.changedTouches[0].screenX;
+      clearInterval(sliderTimer);
+    });
+  });
 
-        slide.addEventListener('touchstart', (e) => {
-            e.preventDefault();
-            touchStart = e.changedTouches[0].screenX;
-            clearInterval(sliderTimer);
-        });
+  slides.forEach((slide) => {
+    slide.addEventListener('mouseleave', () => {
+      sliderTimer = setInterval(() => {
+        changeSlide(++dotsCounter);
+      }, 5000);
     });
 
-    slides.forEach(slide => {
-        slide.addEventListener('mouseleave', () => {
-            sliderTimer = setInterval(() => {
-                changeSlide(++dotsCounter);
-            }, 5000);
-        });
+    slide.addEventListener('touchend', (e) => {
+      e.preventDefault();
+      touchEnd = e.changedTouches[0].screenX;
+      changeSlideByTouch();
+      sliderTimer = setInterval(() => {
+        changeSlide(++dotsCounter);
+      }, 5000);
+    });
+  });
 
-        slide.addEventListener('touchend', (e) => {
-            e.preventDefault();
-            touchEnd = e.changedTouches[0].screenX;
-             changeSlideByTouch();
-             sliderTimer = setInterval(() => {
-                changeSlide(++dotsCounter);
-            }, 5000);
-         });
+  function changeSlide(n) {
+    dotsCounter = n;
+
+    dotsCounter = checkCounter(dotsCounter, slides.length - 1);
+
+    dots.forEach((dot) => {
+      dot.classList.remove('dot_active');
     });
 
-    function changeSlide(n) {
-        dotsCounter = n;
+    dots[dotsCounter].classList.add('dot_active');
 
-        dotsCounter = checkCounter(dotsCounter, slides.length - 1);
-       
-        dots.forEach(dot => {
-            dot.classList.remove('dot_active');
-        });
+    slides.forEach((slide) => {
+      slide.style.opacity = '0';
+    });
 
-        dots[dotsCounter].classList.add('dot_active');
+    slides[dotsCounter].style.opacity = '1';
+  }
 
-        slides.forEach(slide => {
-            slide.style.opacity = '0';
-        });
-
-        slides[dotsCounter].style.opacity = '1';
-    };
-
-    dotsContainer.addEventListener('click', (e) => {
+  dotsContainer.addEventListener('click', (e) => {
     const target = e.target;
 
     dots.forEach((dot, i) => {
-        if (target && dot === target) {
-                changeSlide(i);
-            };
-        });
+      if (target && dot === target) {
+        changeSlide(i);
+      }
     });
+  });
 
-    dots[0].click();
-
+  dots[0].click();
 };
 
 export default reviewsSlider;
